0d05795f1ab132ece6941f1046dccd576559d0ad
fanhl <dyguests@qq.com>
2016-07-08 10:06:57 +0800

    chartPage

---
+++ ChartPage.js
@@ -0,0 +1,45 @@
+/**
+ * Created by fanhl on 16/7/8.
+ */
+'use strict';//使用严格模式
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    View,
+} from 'react-native';
+import Chart from 'react-native-chart';
+
+const data = [
+    [0, 1],
+    [1, 3],
+    [3, 7],
+    [4, 9],
+];
+
+export default class ChartPage extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Chart
+                    style={styles.chart}
+                    data={data}
+                    verticalGridStep={5}
+                    type="line"/>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    chart: {
+        width: 200,
+        height: 200,
+    },
+});
--- index.ios.js
+++ index.ios.js
@@ -3,48 +3,11 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+'use strict';//使用严格模式
 import React, {Component} from 'react';
 import {
     AppRegistry,
-    StyleSheet,
-    Text,
-    View
 } from 'react-native';
-import Chart from 'react-native-chart';
+import ChartPage from './ChartPage';
 
-const data = [
-    [0, 1],
-    [1, 3],
-    [3, 7],
-    [4, 9],
-];
-
-class Chart1 extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Chart
-                    style={styles.chart}
-                    data={data}
-                    verticalGridStep={5}
-                    type="line"/>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-    },
-    chart: {
-        width: 200,
-        height: 200,
-    },
-});
-
-AppRegistry.registerComponent('Chart1', () => Chart1);
+AppRegistry.registerComponent('Chart1', () => ChartPage);
